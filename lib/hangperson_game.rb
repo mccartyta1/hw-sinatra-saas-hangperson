4f60438cb394794fe1032a0cdc4f91698512e40f
Tristan <mccartyta1@g.cofc.edu>
2016-09-27 17:35:27 +0000

    Doot doot

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,12 +5,52 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  #def initialize()
+  #end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
-    @word = word
+    @word = word.downcase ## Just in case
+    @guesses = ""
+    @wrong_guesses = ""
   end
+  
+  def guess(x)
+    if x == nil || x.length != 1 || x =~ /[^a-z]/i
+      raise ArgumentError.new
+    end
+    x = x.downcase
+    if @wrong_guesses.include?(x)|| @guesses.include?(x)
+      return false
+    end
+    if word.include? x
+      @guesses << x
+    else
+      @wrong_guesses << x
+    end
+    return true
+  end
+  
+  def word_with_guesses
+    if guesses.length < 1
+      exp = /[#{Regexp.quote(@word)}]/
+    else
+      exp = /[^#{Regexp.quote(@guesses)}]/
+    end
+    
+    return word.gsub(exp ,  '-')
+  end
+  
+  def check_win_or_lose
+    if word_with_guesses == @word
+      return :win
+    elsif @wrong_guesses.length == 7
+      return :lose
+    else
+      return :play
+    end
+  end
+  
 
   def self.get_random_word
     require 'uri'
