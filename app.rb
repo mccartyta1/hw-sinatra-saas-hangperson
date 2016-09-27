aff56fc8901221ca2b92d50a43d2d69729cafce4
Tristan <mccartyta1@g.cofc.edu>
2016-09-27 18:46:48 +0000

    Everything is supes g

--- app.rb
+++ app.rb
@@ -40,6 +40,15 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    
+    begin
+    if @game.guess(letter) == false
+      flash[:message] = "You have already used that letter."
+    end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
+    
     redirect '/show'
   end
   
@@ -50,16 +59,27 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose != :play
+      erb @game.check_win_or_lose
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :win
+      redirect '/show'
+    end
+    
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
+    if @game.check_win_or_lose != :lose
+      redirect '/show'
+    end
     erb :lose # You may change/remove this line
   end
   
